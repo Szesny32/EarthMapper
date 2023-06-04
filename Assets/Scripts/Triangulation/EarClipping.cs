6ae200349a6b16b6b4ec95d788b585e50141b957
Szesny32 <bartoszszczesny.studia@gmail.com>
2023-06-04 21:15:15 +0200

    ear clipping table initialization

--- Assets/Scripts/Triangulation/EarClipping.cs
+++ Assets/Scripts/Triangulation/EarClipping.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+
+    public struct EarClippingProcessData{
+        public bool isCounterClockwiseOrder;
+        public HashSet<int> convexVertices;
+        public HashSet<int> reflexVertices;
+        public HashSet<int> earVertices;
+    };
+
+
+
 public class EarClipping : MonoBehaviour
 {
 
 
-     public Dictionary<string, OsmNode> nodesDictionary;
+    public Dictionary<string, OsmNode> nodesDictionary;
+
+
+
 
     void Start()
     {
@@ -54,18 +67,10 @@ public class EarClipping : MonoBehaviour
         nodesDictionary.Add("9", p9);
 
         OsmWay way;
-        way.osmNodes = Enumerable.Range(0, 10).Select(i => (9-i).ToString()).ToList();
+        way.osmNodes = Enumerable.Range(0, 10).Select(i => (i).ToString()).ToList();
         way.category = "none";
         way.type = "none";
-
-if(isClockwiseOrder(way, nodesDictionary)){
-    Debug.Log("Clockwise");
-} else {
-    Debug.Log("CounterClockwise");
-}
-        
-        getTriangles(way, nodesDictionary);
-
+        initTriangulationData(way, nodesDictionary);
     }
 
     // Update is called once per frame
@@ -73,28 +78,44 @@ if(isClockwiseOrder(way, nodesDictionary)){
     {
         
     }
+ 
+    public EarClippingProcessData initTriangulationData(OsmWay way,  Dictionary<string, OsmNode> nodesDictionary){
+        EarClippingProcessData processData;
+        processData.isCounterClockwiseOrder = isCounterClockwiseOrder(way, nodesDictionary);
+        processData.convexVertices = new HashSet<int>();
+        processData.reflexVertices = new HashSet<int>();
+        processData.earVertices = new HashSet<int>();
+        Debug.Log(processData.isCounterClockwiseOrder? "Counter-clockwise" : "Clockwise" );
 
-    public void getTriangles(OsmWay way, Dictionary<string, OsmNode> nodesDictionary){
-        for(int i = 0; i < way.osmNodes.Count; i++){
+            for(int i = 0; i < way.osmNodes.Count; i++){
+                int prev = (i - 1 + way.osmNodes.Count) % way.osmNodes.Count;
+                int next = (i + 1 + way.osmNodes.Count) % way.osmNodes.Count;
 
-            int prev = (i - 1 + way.osmNodes.Count) % way.osmNodes.Count;
-            int next = (i + 1 + way.osmNodes.Count) % way.osmNodes.Count;
+                Vector3 AM = nodesDictionary[way.osmNodes[prev]].point - nodesDictionary[way.osmNodes[i]].point;
+                Vector3 BM = nodesDictionary[way.osmNodes[next]].point - nodesDictionary[way.osmNodes[i]].point;
 
-            Vector3 AM = nodesDictionary[way.osmNodes[prev]].point - nodesDictionary[way.osmNodes[i]].point;
-            Vector3 BM = nodesDictionary[way.osmNodes[next]].point - nodesDictionary[way.osmNodes[i]].point;
+                float crossY = Vector3.Cross(AM, BM).y;
 
-            if (Vector3.Cross(AM, BM).y <= 0f)
-                Debug.Log("V"+way.osmNodes[i] + " jest wklęsły.");
-            else
-                Debug.Log("V"+way.osmNodes[i] + " jest wypukły.");
+                if (processData.isCounterClockwiseOrder? (crossY >= 0f ): (crossY < 0f)){
+                    processData.convexVertices.Add(i);
+                    if(isEar(i, way, nodesDictionary)){
+                        processData.earVertices.Add(i);
+                    }
+                }
+                    //Debug.Log("V"+way.osmNodes[i] + " jest wklęsły.");
+                else
+                    processData.reflexVertices.Add(i); //Debug.Log("V"+way.osmNodes[i] + " jest wypukły.");
+            }
+
+            Debug.Log("Convex vertices: "+string.Join(", ", processData.convexVertices));
+            Debug.Log("Reflex vertices: "+string.Join(", ", processData.reflexVertices));
+            Debug.Log("Ear vertices: "+string.Join(", ", processData.earVertices));
             
-          
 
-        }
-        
+        return processData;
     }
 
-    public bool isClockwiseOrder(OsmWay way,  Dictionary<string, OsmNode> nodesDictionary){
+    public bool isCounterClockwiseOrder(OsmWay way,  Dictionary<string, OsmNode> nodesDictionary){
         float area = 0f;
         for(int i = 0; i < way.osmNodes.Count; i++){
             int j = (i + 1) % way.osmNodes.Count;
@@ -104,5 +125,40 @@ if(isClockwiseOrder(way, nodesDictionary)){
         return (area > 0f);
     }
 
+    public bool isEar(int id, OsmWay way,  Dictionary<string, OsmNode> nodesDictionary){
+        int previousId = (id-1 + way.osmNodes.Count) % way.osmNodes.Count;
+        int nextId = (id+1 + way.osmNodes.Count) % way.osmNodes.Count;
+        
+        Vector3 v0 = nodesDictionary[way.osmNodes[previousId]].point;
+        Vector3 v1 = nodesDictionary[way.osmNodes[id]].point;
+        Vector3 v2 = nodesDictionary[way.osmNodes[nextId]].point;
+
+        for (int i = 0; i < way.osmNodes.Count; i++){
+
+            if(i == previousId || i == id || i == nextId){
+                continue;
+            }
+            Vector3 v = nodesDictionary[way.osmNodes[i]].point;
+
+
+            float d1 = sign(v, v0, v1);
+            float d2 = sign(v, v1, v2);
+            float d3 = sign(v, v2, v0);
+
+            bool hasNeg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+            bool hasPos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+            if(!(hasNeg && hasPos))
+                return false;
+            
+
+        }
+        return true;
+    }
+
+
+    private float sign(Vector3 v0, Vector3 v1, Vector3 v2){
+        return (v0.x - v2.x) * (v1.z - v2.z) - (v1.x - v2.x) * (v0.z - v2.z);
+    }
 
 }
